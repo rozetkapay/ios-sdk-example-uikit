2105674b8f62d08e15ca793be498fb89488323eb
casiocompa <casiocompa@gmail.com>
2024-09-20 18:28:32 +0200

    Initial Commit

---
+++ RozetkaPay.Example.UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RozetkaPay.Example.UIKit
+//
+//  Created by Ruslan Kasian Dev on 20.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
